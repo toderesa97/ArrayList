630eedf9e54c1201a2dbae342a3f5bf47fbd71e9
Thomas DRS <toderesa97@gmail.com>
2017-02-08 22:38:22 +0000

    Add files via upload
    Added new example of using this custom container, note that you can specify in toString() method how lines do you want to print if the content is large.

--- src/UseContainers.java
+++ src/UseContainers.java
@@ -1,23 +1,40 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class UseContainers {
 
     public static void main(String[] args) {
-        Lista<String> expected = new ArrayLista<>();
-        expected.add("Hey");
-        expected.add("makey");
-        expected.add("How");
-        expected.add("are");
-        expected.add("you?");
+        Lista<Paciente> pacienteLista = new ArrayLista<>(Paciente::compareTo);
+        pacienteLista.add(new Paciente(21, "Jesus"));
+        pacienteLista.add(new Paciente(22, "Pepe"));
+        pacienteLista.add(new Paciente(22, "Maria"));
+        pacienteLista.add(new Paciente(28, "Bob"));
+        pacienteLista.add(new Paciente(18, "Jerry"));
+        pacienteLista.add(new Paciente(41, "Melissa"));
+        pacienteLista.add(new Paciente(34, "Cristal"));
 
-        Lista<String> ls = expected.filter(string->string.contains("y"));
-
-        for (int i = 0; i < ls.size(); i++) {
-            System.out.print(ls.get(i)+",");
-        }
-
+        System.out.println(pacienteLista.toString(1));
     }
 
+
+
+}
+
+class Paciente implements Comparable<Paciente>{
+    int age;
+    String name;
+
+    Paciente(int age, String name){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Patient called %s is %s", name, age);
+    }
+
+    @Override
+    public int compareTo(Paciente paciente){
+        return age == paciente.age ? name.compareTo(paciente.name) :
+                age-paciente.age;
+    }
 }
